ba70329e4c00d822a45792621147012306debd55
Martin Bogomolni <martinbogo@atxhs.org>
2019-01-29 11:20:29 -0600

    Looking at registers 12/204 Solved temperature sensor, will merge to main

--- SegwayBatteryDiag/SegwayBatteryDiag.ino
+++ SegwayBatteryDiag/SegwayBatteryDiag.ino
@@ -6,7 +6,7 @@
    Created 15 Jan, 2019
    Updated 27 Jan, 2018
 
-   v 1.10
+   v 1.20
 
    This code is copyright 2019, and under the MIT License
 
@@ -34,9 +34,14 @@
 // For Lithium Segway Batteries uncomment these lines
 #define TYPE 0x31
 #define CELLGROUPS 23
+#define TEMPSENSORS 4
 // For NiMH Segway Batteries uncomment these lines
 //#define TYPE 0x62
 //#define CELLGROUPS 6
+//#define TEMPSENSORS 4
+
+int curiousregs[4] = { 12, 204 };
+int unknownregs[1] = { 198 };
 
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
@@ -47,6 +52,64 @@ void loop() {
   introMessage();
   while (1) {
     doMenu();
+  }
+}
+
+void readRegs(void) {
+  unsigned char msb[128];
+  unsigned char lsb[128];
+  for ( unsigned int scan = 0; scan < 2; scan++ ) {
+    Serial.print("Register <");
+    Serial.print(curiousregs[scan]);
+    Serial.println("> ");
+    for ( unsigned int itor = 0; itor < 31; itor++ ) {
+      Wire.beginTransmission(TYPE);
+      Wire.write(curiousregs[scan]);
+      Wire.endTransmission();
+      Wire.requestFrom(TYPE, 3);
+      while (Wire.available()) {
+        unsigned char a = Wire.read();
+        unsigned char b = Wire.read();
+        unsigned char c = Wire.read();
+        msb[itor] = b;
+        lsb[itor] = c;
+      }
+    }
+    for ( int u = 0; u < 31; u++ ) {
+      printBits(msb[u]);
+      Serial.print(" ");
+      printBits(lsb[u]);
+      Serial.print(" ");
+      Serial.print(msb[u],HEX);
+      Serial.print(" ");
+      Serial.print(lsb[u],HEX);
+      Serial.println();
+    }
+  }
+}
+
+void readTemps(void) {
+  for (int i = 0; i < TEMPSENSORS; i++) {
+    Wire.beginTransmission(TYPE);
+    Wire.write(0x17); // 0x17 and 0xD7 hold the temp sensor values, mirrored registers!
+    Wire.endTransmission();
+
+    Wire.requestFrom(TYPE, 3);
+    while (Wire.available()) {
+      char t = Wire.read(); // throwaway checksum
+      delay(5);
+      char h = Wire.read(); // high byte
+      delay(5);
+      char l = Wire.read(); // low byte
+      unsigned int sensornum = word(h, l) >> 12; // top 4 bits are sensor number
+      unsigned int rawtemp = word(h, l) & 0xFFF; // mask off 12 bits ADC
+      float ctemp = rawtemp * 0.0625; // Sensor -256C to +256C scaled by 4096
+      Serial.print("Temp Sensor [");
+      Serial.print(sensornum);
+      Serial.print("] ");
+      Serial.print("Temperature in deg C: ");
+      Serial.println(ctemp);
+    }
   }
 }
 
@@ -66,18 +129,26 @@ void readVoltages(void) {
       char l = Wire.read();
       delay(5);
       unsigned int battery = word(h, l) >> 11; // top 5 bits are cell group
-      unsigned int voltage = word(h, l) & 0x3FF; // 11 bits of ADC
-      long cellvoltage = voltage*7.820; // Scale 8000 milivolts by 1023 steps
+      int voltage = word(h, l) & 0x3FF; // 10 bits of ADC
+      if ( voltage == 0x3ff ) {
+        voltage = -1;
+      }; // 0x3FF means a completely dead group
+      long cellvoltage = voltage * 7.820; // Scale 8000 milivolts by 1023 steps
       Serial.print("Cell Group [");
       Serial.print(battery);
       Serial.print("] ");
       Serial.print("Voltage millivolt: ");
-      Serial.println(cellvoltage);
+      if ( voltage < 0 ) {
+        Serial.println("DEAD");
+        cellvoltage =  0;
+      } else {
+        Serial.println(cellvoltage);
+      }
       packvoltage = packvoltage + (long)cellvoltage;
     }
   }
   Serial.print("Pack voltage: ");
-  Serial.print((float)packvoltage/1000);
+  Serial.print((float)packvoltage / 1000);
   Serial.println(" V");
 }
 
@@ -95,7 +166,7 @@ void readDebugVoltages(void) {
       delay(5);
       unsigned int l = Wire.read();
       delay(5);
-      unsigned int voltage = ((h << 8) + l) & 0x3FF; // 10 bit AD for Voltage
+      unsigned int voltage = ((h << 8) + l) & 0x7FF; // 11 bit AD for Voltage
       unsigned int battery = ((h << 8) + l) >> 11; // top 5 bits are cell group
       Serial.print("Cell Group [");
       Serial.print(battery);
@@ -115,9 +186,9 @@ void readDebugVoltages(void) {
       Serial.print("], Word binary [0b");
       Serial.print((h << 8) + l, BIN);
       Serial.print("] 10 bit voltage binary/decimal [0b");
-      Serial.print(((h << 8) + l) & 0x7FF, BIN);
+      Serial.print(((h << 8) + l) & 0x3FF, BIN);
       Serial.print("]/[");
-      Serial.print(((h << 8) + l) & 0x7FF);
+      Serial.print(((h << 8) + l) & 0x3FF);
       Serial.println("]");
     }
   }
@@ -142,15 +213,33 @@ void introMessage(void) {
 void doMenu(void) {
   Serial.println("V) Read raw cell group voltages");
   Serial.println("D) Read raw cell group voltages with lots of debug Info");
+  Serial.println("T) Read temperature sensors");
+  Serial.println("Q) Query some interesting registers");
   Serial.println("");
   Serial.println("Press key to select menu item:");
   for (;;) {
-    switch (Serial.read()) {
-      case 'V': readVoltages(); break;
-      case 'v': readVoltages(); break;
-      case 'D': readDebugVoltages(); break;
-      case 'd': readDebugVoltages(); break;
-      default: continue;
+    if (Serial.available() > 0) {
+      int inByte = Serial.read();
+      switch (inByte) {
+        case 'V': readVoltages(); break;
+        case 'v': readVoltages(); break;
+        case 'D': readDebugVoltages(); break;
+        case 'd': readDebugVoltages(); break;
+        case 'T': readTemps(); break;
+        case 't': readTemps(); break;
+        case 'Q': readRegs(); break;
+        case 'q': readRegs(); break;
+        default: continue;
+      }
     }
   }
+}
+
+void printBits(byte myByte){
+ for(byte mask = 0x80; mask; mask >>= 1){
+   if(mask  & myByte)
+       Serial.print('1');
+   else
+       Serial.print('0');
+ }
 }
